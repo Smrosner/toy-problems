6875e43e7278868b17bfb70d3c3811f4e284ced6
Shay Rosner <shaymrosner@gmail.com>
2021-06-02 19:18:08 -0700

    Time: 164 ms (15.34%), Space: 51.3 MB (34.09%) - LeetHub

--- count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.js
+++ count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.js
@@ -2,28 +2,13 @@
  * @param {string} allowed
  * @param {string[]} words
  * @return {number}
- I: string of individual lowercase chars, array of string lowercase chars
- O: number 
- C: words has at least 1 element, allowed is at least 1 uniq char, word is at least 1 char
- E:
  */
 var countConsistentStrings = function(allowed, words) {
-    let count = 0
-    for (let i = 0; i < words.length; i++) {
-        let word = words[i]
-        let isAllowed;
-        for (let j = 0; j < word.length; j++) {
-            let letter = word[j]
-            if (allowed.includes(letter)) {
-                isAllowed = true
-            } else {
-                isAllowed = false
-                break
-            } 
-        }
-        if (isAllowed) {
-            count++    
-        }
-    }
-    return count
+  const letters = new Set(allowed);
+  return words.reduce((total, word) => {
+    if (word.split('').every(letter => letters.has(letter))) {
+        total++;
+    };
+    return total;
+  }, 0);
 };
