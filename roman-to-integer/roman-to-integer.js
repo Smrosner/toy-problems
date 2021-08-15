01386e7456bacbeb2736cec2ae12cf4b25c956ae
Shay Rosner <shaymrosner@gmail.com>
2021-08-14 23:22:31 -0700

    Time: 152 ms (49.19%), Space: 44.2 MB (82.29%) - LeetHub

--- roman-to-integer/roman-to-integer.js
+++ roman-to-integer/roman-to-integer.js
@@ -3,30 +3,52 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    const roman = {
-        'I': 1, 
-        'V': 5, 
-        'X': 10, 
-        'L': 50, 
-        'C': 100, 
-        'D': 500, 
-        'M': 1000
-    }
+    // create sum 
+  var sum = 0;
 
-    if (s.length === 1) {
-        return roman[s]
-    }
-       
-    let count = 0
-    
-    for (let i = 0; i < s.length; i++) {
-        let left = s[i]
-        let right = s[i + 1]
-        if (roman[left] < roman[right]) {
-            count -= roman[left]
+  // loop over string
+  for (var i = 0; i < s.length; i++) {
+    // if current string is certain letter
+    var letter = s[i];
+    var letterAfter = s[i+1]
+      // add corrisponding number to sum
+      //MCMXCIX
+      // 1000, 
+      if (letter === 'M') {
+        sum += 1000
+      // 900, 400, 100
+      } else if (letter === 'C') {
+        if (letterAfter === 'M' || letterAfter === 'D') {
+          sum += (-100); // (-100) + 1000;
         } else {
-            count += roman[left]
+          sum += 100;
         }
-    }
-    return count
+      // 500
+      } else if (letter === 'D') {
+        sum += 500
+      // 50
+      } else if (letter === 'L') {
+        sum += 50
+      // 90, 40, 10
+      } else if (letter === 'X') {
+        if (letterAfter === 'C' || letterAfter === 'L') {
+          sum += (-10); // (-10) + 50;
+        } else {
+        sum += 10
+        }
+      // 5
+      } else if (letter === 'V') {
+        sum += 5;
+      // 9, 4, 1
+      } else if (letter === 'I') {
+        if (letterAfter === 'X' || letterAfter === 'V') {
+          sum += (-1);
+        } else {
+        sum += 1
+        }
+      }
+  }
+
+  // return sum
+  return sum;
 };
