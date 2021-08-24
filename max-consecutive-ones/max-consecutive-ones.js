73036c2e8903a58ec5748c3715653202198491ca
Shay Rosner <shaymrosner@gmail.com>
2021-08-23 22:06:07 -0700

    Time: 60 ms (100.00%), Space: 41.4 MB (61.68%) - LeetHub

---
+++ max-consecutive-ones/max-consecutive-ones.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+   if (nums.length === 1) {
+        if (nums[0] === 1) {
+            return 1
+        } else {
+            return 0
+        }
+    }
+
+    let count = 0
+    let result = 0
+    
+    for (let i = 0; i < nums.length; i++) {
+       if (nums[i] === 1) {
+           count += 1
+       } else {
+           result = Math.max(result,count)
+           count = 0
+       }
+    }
+
+    return  Math.max(result,count)
+};
