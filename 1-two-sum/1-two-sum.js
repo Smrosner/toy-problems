675402c8af9d04bd3bb94276bb0c7e43ec334b70
Shay Rosner <shaymrosner@gmail.com>
2022-04-25 22:42:51 -0700

    Time: 190 ms (19.48%), Space: 42.4 MB (71.89%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -2,30 +2,21 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
- 
- i- nums = array of numbers, target, number
- o- an array of two numbers that add up to target number in any order
- c- You may assume that each input would have exactly one solution, and you may not use the same element twice.
- e- 
- 
- nums= [3,2,4], target 6
- look at each number moving from left to right to see if any number adds up target, if not by end move to next in line and search again.
- when two numbers do add up to target, add their index to array and return  
+ I: array of nums, target int 
+ O: indices of two numbers, that add up to target
+ C: only 1 valid answer, at least 2 length
+ E: if length of 2 return array of [0,1]
  */
 var twoSum = function(nums, target) {
-
     if (nums.length === 2) {
         return [0,1]
-    }
-
-    for (i = 0; i < nums.length; i++) {
-        for (j = 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                if (i !== j) {
-                    return [i,j]
-                }
-                
-            } 
+    }    
+    
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i!==j && (nums[i] + nums[j] === target)) {
+                return [i,j]
+            }
         }
     }
 
