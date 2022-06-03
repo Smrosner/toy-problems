0733094d69e7cc0f4b88d0d523f51cd22b219998
Shay Rosner <shaymrosner@gmail.com>
2022-06-03 12:57:09 -0700

    Time: 77 ms (67.79%), Space: 44.7 MB (87.97%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -2,22 +2,6 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
- 
- [-1,0,3,5,9,12] = 6 length
- 
- left 0 aka -1 , right 5 aka 12
- middle = 5 - 0 / 2 floored = 2
- nums[2] = 3 3 is less than 9 target
- left = middle + 1 aka 2 + 1 = 3
-
- left 3 aka 5 , right 5 aka 12
- middle = 5 - 3 / 2 = 1
-
- nums[1] = 9 
- 9 is target return middle or 1
- 
- 
- 
  */
 var search = function(nums, target) {
     
