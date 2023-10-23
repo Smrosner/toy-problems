c121a5af1d06856f261ab4b35f9e5e33aa089e47
Shay Rosner <shaymrosner@gmail.com>
2023-10-22 14:53:45 -1000

    Time: 55 ms (58.44%), Space: 44.5 MB (93.24%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > target) {
+            return -1;
+        }
+
+        if (nums[i] === target) {
+            return i;
+        }
+    }
+    
+    return -1;
+    
+};
